e9c33152a3b30124b43958583afac2ebed98ed1c
surabent <mazathanu71@gmail.com>
2025-11-10 18:20:30 +0700

    feat: remove user login feature

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { SessionProvider } from "next-auth/react";
 import { Silkscreen } from "next/font/google";
 import "./globals.css";
 
@@ -20,12 +19,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <SessionProvider>
-        <body className={`${silkscreen.className} antialiased`}>
-          {children}
-          <div className="animate-background-x"></div>
-        </body>
-      </SessionProvider>
+      <body className={`${silkscreen.className} antialiased`}>
+        {children}
+        <div className="animate-background-x"></div>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
 "use client";
-import { signIn } from "next-auth/react";
-import { useState } from "react";
+import { useRouter } from "next/navigation";
 export default function Home() {
-  const [isClicked, setIsClicked] = useState(false);
+  const router = useRouter();
   return (
     <div className="bg-lofi-one relative flex flex-col space-y-10 p-10 min-h-screen items-center">
       <div className="flex flex-col items-center space-y-2">
@@ -12,14 +11,10 @@ export default function Home() {
         <h4 className="text-lg text-lofi-three">Let's Play Tic-Tac-Toe</h4>
       </div>
       <button
-        disabled={isClicked}
-        onClick={() => {
-          setIsClicked(true);
-          signIn("auth0", { redirectTo: "/play" });
-        }}
+        onClick={() => router.push("/play")}
         className="transition-all linear rounded-xl p-4 bg-lofi-four active:bg-lofi-three active:shadow-[0_5px_#1a1c2c] active:translate-y-1 disabled:bg-lofi-three disabled:shadow-[0_5px_#1a1c2c] disabled:translate-y-1 text-lofi-one shadow-[0_10px_#1a1c2c]"
       >
-        LOGIN
+        PLAY
       </button>
     </div>
   );
